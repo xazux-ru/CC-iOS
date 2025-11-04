dd29059ea885e1a73b776d451573d7bb034a0644
vamspa <48481477+vamspa-off@users.noreply.github.com>
2025-11-04 14:49:32 +0300

    Initial Commit

---
+++ CC/CCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCApp.swift
+//  CC
+//
+//  Created by macbook on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CC
+//
+//  Created by macbook on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
